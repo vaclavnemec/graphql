6fd5876e504efab297c164b718debbde1b0489cc
Václav Němec <vaclavnemec@email.cz>
2019-08-23 00:45:11 +0200

    Removing controller as the servlet will be auto configured

--- src/main/java/com/example/graphql/GraphQLApplication.java
+++ src/main/java/com/example/graphql/GraphQLApplication.java
@@ -1,7 +1,16 @@
 package com.example.graphql;
 
+import com.example.graphql.book.Book;
+import com.example.graphql.book.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class GraphQLApplication {
@@ -10,4 +19,19 @@ public class GraphQLApplication {
 		SpringApplication.run(GraphQLApplication.class, args);
 	}
 
+	@Autowired
+	BookRepository bookRepository;
+
+	@Bean
+	CommandLineRunner loadIntoDb() {
+		return runner -> bookRepository.saveAll(
+				Stream.of(
+						new Book("isbn1", "Freedom fighter",
+								"Mohen", new String[] {"Jurecka"}, new Date()),
+						new Book("isbn2", "Freedom fighter 2",
+								"Mohen", new String[] {"Jurecka"}, new Date()),
+						new Book("isbn3", "Freedom fighter 3",
+								"Mohen", new String[] {"Jurecka"}, new Date()))
+						.collect(Collectors.toList()));
+	}
 }
--- src/main/java/com/example/graphql/GraphQLConfiguration.java
+++ src/main/java/com/example/graphql/GraphQLConfiguration.java
@@ -2,63 +2,40 @@ package com.example.graphql;
 
 import com.example.graphql.book.AllBooksDataFetcher;
 import com.example.graphql.book.BookDataFetcher;
-import com.example.graphql.book.Book;
 import com.example.graphql.book.BookRepository;
-import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.util.Date;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-@Service
-public class GraphQLService {
+@Configuration
+public class GraphQLConfiguration {
 
     private final Resource resource;
-    private GraphQL graphQL;
-
     private final AllBooksDataFetcher allBooksDataFetcher;
     private final BookDataFetcher bookDataFetcher;
     private final BookRepository bookRepository;
 
-    public GraphQLService(@Value("classpath:books.graphql") Resource resource,
-                          AllBooksDataFetcher allBooksDataFetcher, BookDataFetcher bookDataFetcher, BookRepository bookRepository) {
+    public GraphQLConfiguration(@Value("classpath:books.graphql") Resource resource,
+                                AllBooksDataFetcher allBooksDataFetcher, BookDataFetcher bookDataFetcher, BookRepository bookRepository) {
         this.resource = resource;
         this.allBooksDataFetcher = allBooksDataFetcher;
         this.bookDataFetcher = bookDataFetcher;
         this.bookRepository = bookRepository;
     }
 
-    @PostConstruct
-    public void loadSchema() throws IOException {
-
-        loadIntoDb();
-
+    @Bean
+    public GraphQLSchema schema() throws IOException {
         TypeDefinitionRegistry typeDefinitionRegistry = new SchemaParser().parse(resource.getFile());
         RuntimeWiring runtimeWiring = buildRuntimeWiring();
-        GraphQLSchema schema = new SchemaGenerator().makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
-        graphQL = GraphQL.newGraphQL(schema).build();
-    }
-
-    private void loadIntoDb() {
-        bookRepository.saveAll(
-                Stream.of(
-                        new Book("isbn1", "Freedom fighter",
-                                "Mohen", new String[] {"Jurecka"}, new Date()),
-                        new Book("isbn2", "Freedom fighter 2",
-                                "Mohen", new String[] {"Jurecka"}, new Date()),
-                        new Book("isbn3", "Freedom fighter 3",
-                                "Mohen", new String[] {"Jurecka"}, new Date()))
-                        .collect(Collectors.toList()));
+        return new SchemaGenerator().makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
     }
 
     private RuntimeWiring buildRuntimeWiring() {
@@ -69,9 +46,5 @@ public class GraphQLService {
                             .dataFetcher("allBooks", allBooksDataFetcher)
                             .dataFetcher("book", bookDataFetcher)
                 ).build();
-    }
-
-    public GraphQL getGraphQL() {
-        return graphQL;
     }
 }
